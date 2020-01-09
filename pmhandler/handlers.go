8906ca9bb3a3e790c9ff89aba336d8bcdf23bc0f
Brooke Bryan <brooke@bajb.net>
2020-01-09 15:54:20 +0000

    Correct handlers

--- pmhandler/handlers.go
+++ pmhandler/handlers.go
@@ -5,10 +5,10 @@ import (
 	"github.com/fortifi/productmanager-go/response"
 )
 
-func (h *Handler) SetAvailabilityCheckHandler(handler func(*request.AvailabilityCheck) (response.Availability, error)) {
+func (h *Handler) SetAvailabilityCheckHandler(handler func(*request.AvailabilityCheck) (response.AvailabilityCheck, error)) {
 	h.handlers.availabilityCheck = handler
 }
-func (h *Handler) SetAvailabilityReserveHandler(handler func(*request.AvailabilityReserve) (response.Availability, error)) {
+func (h *Handler) SetAvailabilityReserveHandler(handler func(*request.AvailabilityReserve) (response.AvailabilityReserve, error)) {
 	h.handlers.availabilityReserve = handler
 }
 func (h *Handler) SetProvisionSetupHandler(handler func(*request.ProvisioningSetup) (response.Provisioning, error)) {
@@ -40,8 +40,8 @@ func (h *Handler) SetHealthCheckHandler(handler func(*request.HealthCheck) (resp
 }
 
 type handlers struct {
-	availabilityCheck      func(*request.AvailabilityCheck) (response.Availability, error)
-	availabilityReserve    func(*request.AvailabilityReserve) (response.Availability, error)
+	availabilityCheck      func(*request.AvailabilityCheck) (response.AvailabilityCheck, error)
+	availabilityReserve    func(*request.AvailabilityReserve) (response.AvailabilityReserve, error)
 	provisionSetup         func(*request.ProvisioningSetup) (response.Provisioning, error)
 	provisionActivate      func(*request.ProvisioningActivate) (response.Provisioning, error)
 	provisionPropertiesSet func(*request.ProvisioningPropertiesSet) (response.Provisioning, error)
