df11cc14ff1c60b327694f44dc48ba5d54b8b653
Brooke Bryan <brooke@bajb.net>
2020-01-10 14:03:18 +0000

    pmtime.Now

--- pmtime/time.go
+++ pmtime/time.go
@@ -7,6 +7,6 @@ type Time struct {
 }
 
 func (t *Time) ForTransport() int64 { return t.t.UnixNano() / int64(time.Millisecond) }
-
-func FromTime(t time.Time) Time  { return Time{t: t} }
-func FromTransport(t int64) Time { return Time{t: time.Unix(0, t*int64(time.Millisecond))} }
+func Now() Time                     { return Time{t: time.Now()} }
+func FromTime(t time.Time) Time     { return Time{t: t} }
+func FromTransport(t int64) Time    { return Time{t: time.Unix(0, t*int64(time.Millisecond))} }
