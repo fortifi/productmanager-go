d374baf024a079df1d614896b251355645cc4b30
Brooke Bryan <brooke@bajb.net>
2020-01-10 14:07:55 +0000

    Pointer not req

--- pmtime/time.go
+++ pmtime/time.go
@@ -6,7 +6,7 @@ type Time struct {
 	t time.Time
 }
 
-func (t *Time) ForTransport() int64 { return t.t.UnixNano() / int64(time.Millisecond) }
-func Now() Time                     { return Time{t: time.Now()} }
-func FromTime(t time.Time) Time     { return Time{t: t} }
-func FromTransport(t int64) Time    { return Time{t: time.Unix(0, t*int64(time.Millisecond))} }
+func (t Time) ForTransport() int64 { return t.t.UnixNano() / int64(time.Millisecond) }
+func Now() Time                    { return Time{t: time.Now()} }
+func FromTime(t time.Time) Time    { return Time{t: t} }
+func FromTransport(t int64) Time   { return Time{t: time.Unix(0, t*int64(time.Millisecond))} }
