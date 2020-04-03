0391ce8629b647c9d8b3a1a29f9df09cab2fac4c
Tom Kay <oridan82@gmail.com>
2020-04-03 18:22:28 +0100

    add reserve ttl

--- request/availability.reserve.go
+++ request/availability.reserve.go
@@ -3,11 +3,13 @@ package request
 type AvailabilityReserve struct {
 	Request
 	ReserveKey string `json:"reserveKey"`
+	ReserveTTL int    `json:"reserveTtl"`
 }
 
-func NewAvailabilityReserve(reserveKey string) AvailabilityReserve {
+func NewAvailabilityReserve(reserveKey string, reserveTtl int) AvailabilityReserve {
 	r := AvailabilityReserve{}
 	r.ReserveKey = reserveKey
+	r.ReserveTTL = reserveTtl
 	r.Type = TYPE_AVAILABILITY_RESERVE
 	return r
 }
