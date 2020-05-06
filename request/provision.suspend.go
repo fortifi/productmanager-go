9720ecd12ed34a94e6730852985429ec688dcd9b
Brooke Bryan <brooke@bajb.net>
2020-05-06 23:41:41 +0100

    Add message to new provisioning suspend request

--- request/provision.suspend.go
+++ request/provision.suspend.go
@@ -1,6 +1,9 @@
 package request
 
-type ProvisioningSuspend struct{ Provisioning }
+type ProvisioningSuspend struct {
+	Provisioning
+	Message string `json:"message"`
+}
 
 func NewProvisioningSuspend() ProvisioningSuspend {
 	r := ProvisioningSuspend{}
