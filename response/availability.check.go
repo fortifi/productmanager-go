b324ffc6277c81575f14ea24f4d252a926bded90
Brooke Bryan <brooke@bajb.net>
2020-08-12 10:55:50 +0100

    Availability Suggestions

--- response/availability.check.go
+++ response/availability.check.go
@@ -1,6 +1,9 @@
 package response
 
-import "github.com/fortifi/productmanager-go/pmtime"
+import (
+	"github.com/fortifi/productmanager-go/pmtime"
+	"github.com/fortifi/productmanager-go/transport"
+)
 
 func NewAvailabilityCheck() *AvailabilityCheck {
 	r := &AvailabilityCheck{}
@@ -11,6 +14,12 @@ func NewAvailabilityCheck() *AvailabilityCheck {
 
 type AvailabilityCheck struct {
 	Response
-	IsAvailable   bool     `json:"isAvailable"`
-	AvailableSKUs []string `json:"availableSkus"`
+	IsAvailable   bool                          `json:"isAvailable"`
+	AvailableSKUs []string                      `json:"availableSkus"`
+	Suggestions   []AvailabilityCheckSuggestion `json:"suggestions"`
+}
+
+type AvailabilityCheckSuggestion struct {
+	Sku        string               `json:"sku"`
+	Properties []transport.Property `json:"properties"`
 }
